e472e9339ff6164ea196e38f1cada33f1b7377bc
Codie Petersen <petersen.codie@gmail.com>
2022-04-09 16:29:32 -0700

    Fixes and library updates.
    These are fixes to the libraries.

--- XAYAUnity/Assets/Scripts/MoverTest/WorldManager.cs
+++ XAYAUnity/Assets/Scripts/MoverTest/WorldManager.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 using XAYAMoverGame;
 using UnityEngine.UI;
+using MudHero.XayaCommunication;
 
 public class WorldManager : MonoBehaviour
 {
@@ -12,7 +15,63 @@ public class WorldManager : MonoBehaviour
     public GameObject moverPrefab;
     public XAYAClient client;
     public XAYAConnector connector;
+    public XayaClient mudHeroClient;
     private bool needsUpdate = false;
+
+    [SerializeField]
+    string hostIP, hostPort, gameIP, gamePort, username, password, endPoint;
+
+    void Start()
+    {
+        ConnectionInfo daemonInfo = new ConnectionInfo()
+        {
+            ip = hostIP,
+            port = hostPort,
+            username = username,
+            userpassword = password,
+            walletPassword = "test",
+            endpointPath = endPoint
+        };
+
+        ConnectionInfo wrapperInfo = new ConnectionInfo()
+        {
+            ip = gameIP,
+            port = gamePort,
+            username = username,
+            userpassword = password,
+            walletPassword = "test",
+            endpointPath = endPoint
+        };
+
+        string basePath = Application.dataPath + "\\..\\XayaStateProcessor\\";
+        StateProcessorPathInfo pathInfo = new StateProcessorPathInfo(basePath, "", "database",
+                                                                     "glogs");
+
+        WrapperInitInputs wrapInputs = new WrapperInitInputs()
+        {
+            initCallback = CallbackFunctions.initialCallbackResult,
+            forwardCallback = CallbackFunctions.forwardCallbackResult,
+            rewindCallback = CallbackFunctions.backwardCallbackResult,
+            pathInfo = pathInfo,
+            daemonInfo = daemonInfo,
+            wrapperInfo = wrapperInfo,
+            storageType = "sqlite",
+            gameNamespace = "mv",
+            network = 0
+        };
+        mudHeroClient = new XayaClient();
+        mudHeroClient.lastProcessedBlock = "e5062d76e5f50c42f493826ac9920b63a8def2626fd70a5cec707ec47a4c4651";
+        try { mudHeroClient.ConnectClient(wrapInputs, Logger); }
+        catch (Exception e) { Debug.Log(e.Message); }
+        Debug.Log(mudHeroClient.IsConnected);
+    }
+
+    private void OnApplicationQuit()
+    {
+        mudHeroClient.Disconnect();
+    }
+
+    void Logger(string log) { File.AppendAllLines("DebugLog.txt", new string[] { log }); }
 
     public void SetGameState(GameState state, int currentHeight, int blockCount)
     {
@@ -24,13 +83,17 @@ public class WorldManager : MonoBehaviour
 
     public void Update()
     {
-        if (needsUpdate)
+        if (needsUpdate) 
             UpdatePlayers();
 
-        if (Input.GetKeyDown(KeyCode.I))
+        /*if (Input.GetKeyDown(KeyCode.I))
             connector.LaunchMoverStateProcessor();
         else if (Input.GetKeyDown(KeyCode.C))
-            client.Connect();
+            client.Connect();*/
+        if (Input.GetKeyDown(KeyCode.Escape))
+            mudHeroClient.Disconnect();
+        if (Input.GetKeyDown(KeyCode.N))
+            Debug.Log(mudHeroClient.gameState);
     }
 
     void UpdatePlayers()
